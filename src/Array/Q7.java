8c15838c8d79afa019671dbd25c463638b1b9532
Ankur357 <ankur.gautam_cs20@gla.ac.in>
2022-01-20 12:07:15 +0530

    code

---
+++ src/Array/Q7.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Q7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array-->");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of array");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int c=0;
+        System.out.println("pair of numbers in the array whose product is odd are");
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n ; j++) {
+                if(i!=j) {
+                    if ((arr[i] * arr[j]) % 2 == 1) {
+                        System.out.println("(" + arr[i] + "," + arr[j] + ")");
+                        c++;
+                    }
+                }
+            }
+            System.out.println("No. of pairs whose product is odd are " + c);
+
+        }
+    }
+}
